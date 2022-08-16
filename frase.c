402fa5cff90d5846c1b0ea612497a85b508823e0
Lucas França <lucas.rfranca@gmail.com>
2022-08-16 19:24:29 -0300

    um frase escrita com caracteres especiais

---
+++ frase.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+main () {
+	
+	setlocale(LC_ALL, "Portuguese");
+
+	printf ("Bem vindos ao mundo /Mundo\\ da programação em \"C\"");
+	
+}
