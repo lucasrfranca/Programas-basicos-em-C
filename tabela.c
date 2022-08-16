e74b0322cc5cfa98f92848baf5ef3504ddfe2763
Lucas França <lucas.rfranca@gmail.com>
2022-08-16 19:23:04 -0300

    tabela simples

---
+++ tabela.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main () {
+	
+	printf ("Total    =     100%%\n");
+	printf ("IVA      =     17%%\n");
+	printf ("IRS      =     15%%\n\n");
+	printf ("--------------------\n\n");
+	printf ("Liq.     =      68%%");
+	
+}
